688fb876fc3db4e00e61f3c0841db83451459f1f
Dwait Pandhi <dwait.pandhi@gmail.com>
2025-08-28 13:26:36 -0400

    first prototype

---
+++ Main.gd
@@ -0,0 +1,120 @@
+extends Node2D
+
+var score = 0
+var is_game_active = false
+var high_score = 0
+
+@onready var player = $Player
+@onready var score_label = $UI/ScoreLabel
+@onready var high_score_label = $UI/HighScoreLabel
+@onready var camera = $Camera2D
+
+var game_over_panel
+var final_score_label  
+var restart_button
+var start_label
+
+func _ready():
+	DisplayServer.window_set_size(Vector2i(700, 700))
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_RESIZE_DISABLED, true)
+	create_boundaries()
+	game_over_panel = get_node_or_null("UI/GameOverPanel")
+	final_score_label = get_node_or_null("UI/GameOverPanel/FinalScoreLabel")
+	restart_button = get_node_or_null("UI/GameOverPanel/RestartButton")
+	start_label = get_node_or_null("UI/StartLabel")
+	if game_over_panel:
+		game_over_panel.visible = false
+	if start_label:
+		start_label.text = "Press Space to Start!"
+	if restart_button:
+		restart_button.pressed.connect(restart_game)
+	load_high_score()
+	update_labels()
+
+func create_boundaries():
+	var thickness = 10
+	var size = Vector2(700, 700)
+	var boundaries = [
+		[Vector2(size.x / 2, -thickness / 2), Vector2(size.x, thickness)], # top
+		[Vector2(size.x / 2, size.y + thickness / 2), Vector2(size.x, thickness)], # bottom
+		[Vector2(-thickness / 2, size.y / 2), Vector2(thickness, size.y)], # left
+		[Vector2(size.x + thickness / 2, size.y / 2), Vector2(thickness, size.y)] # right
+	]
+	for boundary in boundaries:
+		var pos = boundary[0]
+		var dim = boundary[1]
+		var area = Area2D.new()
+		var shape = CollisionShape2D.new()
+		var rect = RectangleShape2D.new()
+		rect.extents = dim / 2
+		shape.shape = rect
+		area.position = pos
+		area.add_child(shape)
+		area.connect("body_entered", Callable(self, "_on_boundary_touched"))
+		add_child(area)
+
+func _on_boundary_touched(body):
+	if body == player:
+		game_over()
+
+func update_labels():
+	if score_label:
+		score_label.text = "Current Score: " + str(score)
+	if high_score_label:
+		high_score_label.text = "High Score: " + str(high_score)
+
+func start_game():
+	is_game_active = true
+	if start_label:
+		start_label.visible = false
+	score = 0
+	update_labels()
+
+func game_over():
+	is_game_active = false
+	if score > high_score:
+		high_score = score
+		save_high_score()
+	if final_score_label:
+		final_score_label.text = "Score: " + str(score) + "\nHigh Score: " + str(high_score)
+	if game_over_panel:
+		game_over_panel.visible = true
+	update_labels()
+
+func add_score():
+	if is_game_active:
+		score += 1
+		update_labels()
+
+func restart_game():
+	score = 0
+	is_game_active = false
+	if game_over_panel:
+		game_over_panel.visible = false
+	if start_label:
+		start_label.visible = true
+		start_label.text = "Press Space to Start!"
+	player.reset()
+	update_labels()
+
+func save_high_score():
+	var file = FileAccess.open("user://high_score.txt", FileAccess.WRITE)
+	if file:
+		file.store_string(str(high_score))
+		file.close()
+
+func load_high_score():
+	if FileAccess.file_exists("user://high_score.txt"):
+		var file = FileAccess.open("user://high_score.txt", FileAccess.READ)
+		if file:
+			var text = file.get_as_text().strip_edges()
+			if text.is_valid_int():
+				high_score = int(text)
+			file.close()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		if not is_game_active:
+			start_game()
+		else:
+			add_score()
---
+++ Player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+const FLAP_STRENGTH = -300.0
+const GRAVITY = 800.0
+const MAX_FALL_SPEED = 400.0
+const ROTATION_SPEED = 0.0
+
+var is_alive = true
+var has_started = false
+
+@onready var game_manager = get_parent()
+
+func _ready():
+	position = Vector2(100, get_viewport().size.y / 2)
+
+func _physics_process(delta):
+	if not is_alive:
+		return
+	velocity.y += GRAVITY * delta
+	if velocity.y > MAX_FALL_SPEED:
+		velocity.y = MAX_FALL_SPEED
+	if Input.is_action_just_pressed("ui_accept"):
+		if not has_started:
+			has_started = true
+			game_manager.start_game()
+		flap()
+	move_and_slide()
+	var screen_height = get_viewport().size.y
+	if position.y > screen_height + 50 or position.y < -50:
+		die()
+
+func flap():
+	if is_alive:
+		velocity.y = FLAP_STRENGTH
+
+func die():
+	if is_alive:
+		is_alive = false
+		game_manager.game_over()
+
+func reset():
+	is_alive = true
+	has_started = false
+	position = Vector2(100, get_viewport().size.y / 2)
+	velocity = Vector2.ZERO
+	rotation_degrees = 0
